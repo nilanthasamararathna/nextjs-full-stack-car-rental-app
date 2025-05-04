74370a68d9fae4d5a4e62cd8a1fd67dc1c1d096e
E.Nilantha Samararathna <45939807+nilanthasamararathna@users.noreply.github.com>
2025-05-04 22:08:56 +0530

    integrate clerk authentication

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,10 @@
-import Image from "next/image";
+import { UserButton } from "@clerk/nextjs";
 
 export default function Home() {
   return (
     <div>
       <h2>Subscribe</h2>
+      <UserButton/>
     </div>
   ); 
 }
